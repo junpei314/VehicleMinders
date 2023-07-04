8e0822573e22b6f99330b4f0e7b9e2cc3c3d049b
junpei-osano <osanojunpei@gmail.com>
2023-07-05 02:54:01 +0900

    Create tables:vehicles,users,notifications

---
+++ vehicleminders_app/db/migrate/20230704172404_create_notifications.rb
@@ -0,0 +1,11 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.references :user, foreign_key: true
+      t.references :vehicle, foreign_key: true
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
