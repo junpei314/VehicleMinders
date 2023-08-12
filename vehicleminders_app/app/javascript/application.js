647622886d89adb067e7c905e0c91a9c4f01e6d6
junpei-osano <osanojunpei@gmail.com>
2023-08-13 00:23:59 +0900

    Update top page

--- vehicleminders_app/app/javascript/application.js
+++ vehicleminders_app/app/javascript/application.js
@@ -1,3 +1,18 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "custom/menu"
+import $ from "jquery";
+window.$ = $;
+window.jQuery = $;
+import "slick-carousel";
+
+document.addEventListener("turbo:load", function() {
+  $('.slider-3').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    adaptiveHeight: true,
+  });
+});
